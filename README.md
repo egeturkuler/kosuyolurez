8c1ad0518fc21a8b1513727c13b1de0c37e79e14
Ege Ata Türküler <ege.turkuler@ozu.edu.tr>
2023-09-17 14:57:36 +0300

    Added checkrez.py, the reservation main code. Made by ycd, modified by egeturkuler

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # kosuyolurez
-An automated python code that checks every 15 mins to see if there is an available spot on the reservation system of a football pitch.
+An automated python code that checks every hour to see if there is an available spot on the reservation system of a football pitch.
---
+++ checkrez.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+days_look_ahead = 3
+
+tomorrow = datetime.now().date() + timedelta(days=days_look_ahead)
+date = tomorrow.strftime("%m/%d/%Y")
+
+url = f"https://spor.kadikoy.bel.tr/Ajax/GetAreaReservationList.ashx?facilityId=2&AreaId=7&firstDayOfWeekLong={date}"
+
+response = requests.get(url)
+
+hour_eight = "20:00/21:00"
+hour_nine = "21:00/22:00"
+hour_ten = "22:00/23:00"
+
+
+
+for listing in response.json()["programs"]:
+
+    # statu 0 is reserved, 1 is available
+    if listing["statu"] == 1:
+        if(hour_eight in listing["hourData"]):
+            print(listing["programDateFormatted"], listing["hourData"])
